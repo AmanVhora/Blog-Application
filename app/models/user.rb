0f02ce9fdfbb265e9ca6ba4581a1107399577fb1
Aman Vhora <amanvhora745@gmail.com>
2023-08-23 23:48:10 +0530

    feat: create blog feature with user authorization

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :blogs, dependent: :destroy
   validates :user_name, presence: true, uniqueness: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
   devise_for :users
+  resources :blogs
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
